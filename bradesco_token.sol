1ab90f2787c0e074b7d33468451cf6fe1103edc7
Gustavo Oliveira <50592755+knmsn@users.noreply.github.com>
2023-09-18 13:56:51 -0500

    Update bradesco_token.sol

--- bradesco_token.sol
+++ bradesco_token.sol
@@ -3,95 +3,241 @@ SPDX-License-Identifier: CC-BY-4.0
 (c) Desenvolvido por Jeff Prestes
 This work is licensed under a Creative Commons Attribution 4.0 International License.
 */
+
 pragma solidity 0.8.19;
 
-contract Aluguel {
-    string public locatario;
-    string public locador;
-    uint256 private valor;
-    uint256 constant public numeroMaximoLegalDeAlugueisParaMulta = 3;
-    bool[] public statusPagamento;
-    mapping (uint256 => uint256) public dataPagamento;
-    /*
-    0 - 01/2020 = true
-    1 - 02/2020 = true
-    2 - 03/2020 = true
-    3 - 04/2020 = true
-    */
-    address payable public contaLocador;
-    address public contaLocatario;
+// OpenZeppelin Contracts v4.4.1 (token/ERC20/IERC20.sol)
+/**
+ * @dev Interface of the ERC20 standard as defined in the EIP.
+ */
+interface IERC20 {
 
-    modifier somenteLocador() {
-        require(msg.sender == contaLocador, "somente locador pode realizar essa operacao");
+    /**
+     * @dev Returns the amount of tokens in existence.
+     */
+    function totalSupply() external view returns (uint256);
+
+    /**
+     * @dev Returns the amount of tokens owned by `account`.
+     */
+    function balanceOf(address account) external view returns (uint256);
+
+    /**
+     * @dev Moves `amount` tokens from the caller's account to `recipient`.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transfer(address recipient, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Returns the remaining number of tokens that `spender` will be
+     * allowed to spend on behalf of `owner` through {transferFrom}. This is
+     * zero by default.
+     *
+     * This value changes when {approve} or {transferFrom} are called.
+     */
+    function allowance(address owner, address spender) external view returns (uint256);
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the caller's tokens.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * IMPORTANT: Beware that changing an allowance with this method brings the risk
+     * that someone may use both the old and the new allowance by unfortunate
+     * transaction ordering. One possible solution to mitigate this race
+     * condition is to first reduce the spender's allowance to 0 and set the
+     * desired value afterwards:
+     * https://github.com/ethereum/EIPs/issues/20#issuecomment-263524729
+     *
+     * Emits an {Approval} event.
+     */
+    function approve(address spender, uint256 amount) external returns (bool);
+
+    /**
+     * @dev Moves `amount` tokens from `sender` to `recipient` using the
+     * allowance mechanism. `amount` is then deducted from the caller's
+     * allowance.
+     *
+     * Returns a boolean value indicating whether the operation succeeded.
+     *
+     * Emits a {Transfer} event.
+     */
+    function transferFrom(
+        address sender,
+        address recipient,
+        uint256 amount
+    ) external returns (bool);
+
+    /**
+     * @dev Emitted when `value` tokens are moved from one account (`from`) to
+     * another (`to`).
+     *
+     * Note that `value` may be zero.
+     */
+    event Transfer(address indexed from, address indexed to, uint256 value);
+
+    /**
+     * @dev Emitted when the allowance of a `spender` for an `owner` is set by
+     * a call to {approve}. `value` is the new allowance.
+     */
+    event Approval(address indexed owner, address indexed spender, uint256 value);
+}
+
+/**
+ * @dev Interface for the optional metadata functions from the ERC20 standard.
+ *
+ * _Available since v4.1._
+ */
+interface IERC20Metadata is IERC20 {
+    /**
+     * @dev Returns the name of the token.
+     */
+    function name() external view returns (string memory);
+
+    /**
+     * @dev Returns the symbol of the token.
+     */
+    function symbol() external view returns (string memory);
+
+    /**
+     * @dev Returns the decimals places of the token.
+     */
+    function decimals() external view returns (uint8);
+}
+
+/// @title Manages the contract owner
+contract Owned {
+    address payable contractOwner;
+
+    modifier onlyOwner() {
+        require(msg.sender == contractOwner, "only owner can perform this operation");
         _;
     }
 
-    constructor(    string memory _nomeLocador, 
-                    string memory _nomeLocatario, 
-                    address _contaLocatario, 
-                    uint256 _valorDoAluguel)  payable {
-        locador = _nomeLocador;
-        locatario = _nomeLocatario;
-        valor = _valorDoAluguel;
-        contaLocador = payable(msg.sender);
-        contaLocatario = _contaLocatario;
+    constructor() { 
+        contractOwner = payable(msg.sender); 
     }
- 
-    function valorAtualDoAluguel() public view returns (uint256) {
-        return valor;
+    
+    function whoIsTheOwner() public view returns(address) {
+        return contractOwner;
     }
- 
-    function simulaMulta( uint256 mesesRestantes, uint256 totalMesesContrato) public view returns(uint256 valorMulta) 
+
+    function changeOwner(address _newOwner) onlyOwner public returns (bool) {
+        require(_newOwner != address(0x0), "only valid address");
+        contractOwner = payable(_newOwner);
+        return true;
+    }
+    
+}
+
+
+/// @title ERC-20 Token template
+contract USDC is IERC20Metadata, Owned {
+    string private myName;
+    string private mySymbol;
+    uint256 private myTotalSupply;
+    uint8 private myDecimals;
+
+    mapping (address=>uint256) balances;
+    mapping (address=>mapping (address=>uint256)) ownerAllowances;
+
+    modifier hasEnoughBalance(address owner, uint amount) {
+        uint balance;
+        balance = balances[owner];
+        require (balance >= amount); 
+        _;
+    }
+
+    modifier isAllowed(address spender, address tokenOwner, uint amount) {
+        require (amount <= ownerAllowances[tokenOwner][spender]);
+        _;
+    }
+
+    modifier tokenAmountValid(uint256 amount) {
+        require(amount > 0);
+        require(amount <= myTotalSupply);
+        _;
+    }    
+
+    constructor() {
+        myName = "BRL Aluno 12";
+        mySymbol = "BRL@Aluno12";
+        myDecimals = 2;
+        mint(msg.sender, (1000000000 * (10 ** myDecimals)));
+    }
+
+    function name() public view returns(string memory) {
+        return myName;
+    }
+
+    function symbol() public view returns(string memory) {
+        return mySymbol;
+    }
+
+    function totalSupply() public override view returns(uint256) {
+        return myTotalSupply;
+    }
+
+    function decimals() public override view returns (uint8) {
+        return myDecimals;
+    }
+
+    function balanceOf(address tokenOwner) public override view returns(uint256) {
+        return balances[tokenOwner];
+    }
+
+    function allowance(address tokenOwner, address spender) public override view returns(uint256) {
+        return ownerAllowances[tokenOwner][spender];
+    }
+
+    function transfer(address to, uint256 amount) 
+    public 
+    override  
+    hasEnoughBalance(msg.sender, amount) 
+    tokenAmountValid(amount) 
+    returns (bool) 
     {
-        valorMulta = valor*numeroMaximoLegalDeAlugueisParaMulta;
-        valorMulta = valorMulta/totalMesesContrato;
-        valorMulta = valorMulta*mesesRestantes;
-        return valorMulta;
+        balances[msg.sender] = balances[msg.sender] - amount;
+        balances[to] = balances[to] + amount;
+        emit Transfer(msg.sender, to, amount);
+        return true;
     } 
+
+    function approve(address spender, uint limit) public override returns(bool) {
+        require(spender != address(0), "ERC20: approve to the zero address");
+        ownerAllowances[msg.sender][spender] = limit;
+        emit Approval(msg.sender, spender, limit);
+        return true;
+    }
+
+    function transferFrom(address from, address to, uint256 amount) public override 
+    hasEnoughBalance(from, amount) isAllowed(msg.sender, from, amount) tokenAmountValid(amount)
+    returns(bool) {
+        balances[from] = balances[from] - amount;
+        balances[to] += amount;
+        ownerAllowances[from][msg.sender] = amount;
+        emit Transfer(from, to, amount);
+        return true;
+    }
+    
+    function mint(address account, uint256 amount) public onlyOwner returns (bool) {
+        require(account != address(0), "ERC20: mint to the zero address");
+
+        myTotalSupply = myTotalSupply + amount;
+        balances[account] = balances[account] + amount;
+        emit Transfer(address(0), account, amount);
+        return true;
+    }
+
+    function burn(address account, uint256 amount) public onlyOwner returns (bool) {
+        require(account != address(0), "ERC20: burn from address");
         
-    function reajustaAluguel(uint256 percentualReajuste) public somenteLocador {        
-        if (percentualReajuste > 20) {
-            percentualReajuste = 20;
-        }
-        uint256 valorDoAcrescimo = 0;
-        valorDoAcrescimo = ((valor*percentualReajuste)/100);
-        valor = valor + valorDoAcrescimo;
-    }
-    
-    function aditamentoValorAluguel(uint256 valorCerto) public somenteLocador  {        
-        valor = valorCerto;
-    }
-
-    function aplicaMulta(uint256 mesesRestantes, uint256 percentual) public     {
-        require(mesesRestantes<30, "Periodo de contrato invalido");
-        for (uint numeroDeVoltas=0; numeroDeVoltas < mesesRestantes; numeroDeVoltas=numeroDeVoltas+2) {
-            valor = valor+((valor*percentual)/100);
-        }
-    }
-    
-    
-    function receberPagamento() public payable {
-        require(msg.value>=valor, "Valor insuficiente");
-        contaLocador.transfer(msg.value);
-        statusPagamento.push(true);
-        dataPagamento[statusPagamento.length] = block.timestamp;
-    }
-    
-    //msg.value = valor em wei enviado ao contrato
-    
-    function retornaTexto(uint256 _parametro) public view returns (string memory) {
-        if ((valor * _parametro) > 5000) {
-            return "Muito caro";
-        } else {
-            return "Preco razoavel";
-        }
-    }
-    
-    function quantosPagamentosJaForamFeitos() public view returns (uint256) {
-        return statusPagamento.length;
-    }
-
-    function quandoPagou(uint256 _parcela) public view returns (uint256) {
-        return dataPagamento[_parcela];
+        balances[account] = balances[account] - amount;
+        myTotalSupply = myTotalSupply - amount;
+        emit Transfer(address(0), account, amount);
+        return true;
     }
 }
