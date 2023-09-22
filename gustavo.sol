3603b183a97af53064b48bf3443a347638ea84e0
Gustavo Oliveira <50592755+knmsn@users.noreply.github.com>
2023-09-22 16:44:46 -0300

    Update gustavo.sol

--- gustavo.sol
+++ gustavo.sol
@@ -2,13 +2,16 @@
 SPDX-License-Identifier: CC-BY-4.0
 (c) Desenvolvido por Gustavo Oliveira
 This work is licensed under a Creative Commons Attribution 4.0 International License.
-*/
 
+https://sepolia.etherscan.io/address/0x690AaC2450d313Dfa82CF421A12d45dDba2339a4
+
+0x690AaC2450d313Dfa82CF421A12d45dDba2339a4
+*/
 pragma solidity 0.8.19;
 
-import "contracts/bradesco_token_aberto.sol";
+import "./bradesco_token_aberto.sol";
 
-contract Token is ExercicioToken {
+contract GORContrato is ExercicioToken {
 
     ExercicioToken public contractAddress;
 
@@ -23,12 +26,12 @@ contract Token is ExercicioToken {
         contractAddress = ExercicioToken(0x89A2E711b2246B586E51f579676BE2381441A0d0);
     }
 
-    function gerarToken(uint256 amount) public onlyOwner {
-        client[msg.sender]._tokenBalance += amount;
+    function saldo() public view returns (uint256) {
+        return contractAddress.balanceOf(msg.sender);
     }
 
-     function saldo() public view returns (uint256) {
-        return contractAddress.balanceOf(msg.sender);
+    function gerarToken(uint256 amount) public onlyOwner {
+        client[msg.sender]._tokenBalance += amount;
     }
 
 }
